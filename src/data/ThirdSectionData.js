1fc0f01631a1219c5b7901602d3b743c3a8efa30
IgorGasic <igor96gasic@gmail.com>
2021-04-05 17:35:45 +0200

    ...

--- src/data/ThirdSectionData.js
+++ src/data/ThirdSectionData.js
@@ -1,6 +1,6 @@
 export const title = 'Savez';
 
-export const text = 'Creative media hub predstavlja savez više udruženja mladih koji se okupljaju oko ideje promovisanja i afirmacije IT sektora u nevladinom sektoru. ....';
+export const text = 'Creative media hub predstavlja savez više udruženja mladih koji se okupljaju oko ideje promovisanja i afirmacije IT sektora u nevladinom sektoru.';
 
 export const img = './images/savez.png';
 
