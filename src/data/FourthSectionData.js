7993be9306f93cf78704e504301d3b0d98ba50c0
IgorGasic <igor96gasic@gmail.com>
2021-04-05 17:33:01 +0200

    ...

--- src/data/FourthSectionData.js
+++ src/data/FourthSectionData.js
@@ -8,21 +8,21 @@ export const data = [
     },
     {
         id:2,
-        name:'Nemanja Sustum',
-        job:'Projektni asistent',
+        name:'Petar Petrovic',
+        job:'Kordinator tima',
         text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt repellendus quibusdam aliquam ipsum iure libero dolorum facilis voluptate explicabo modi eius consequatur quas vero, accusamus eaque, odio ab excepturi quia.',
         img:'./images/02.jpg'
     },
     {
         id:3,
-        name:'Nenad Mokranjac',
-        job:'Projektni asistent',
+        name:'Nikola Nikolic',
+        job:'Kordinator tima',
         text:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, eveniet. Consectetur nihil earum explicabo sunt optio eum doloremque harum accusamus, sint expedita similique et? Doloremque quisquam labore nobis, incidunt dolores maiores asperiores animi. Earum sed numquam illum, quidem sit saepe fuga quo nesciunt at totam suscipit atque dolorum repudiandae nostrum.',
         img:'./images/01.jpg'
     },
     {
         id:4,
-        name:'Dragan Simovic',
+        name:'...',
         job:'Kordinator tima',
         text:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, eveniet. Consectetur nihil earum explicabo sunt optio eum doloremque harum accusamus, sint expedita similique et? Doloremque quisquam labore nobis, incidunt dolores maiores asperiores animi. Earum sed numquam illum, quidem sit saepe fuga quo nesciunt at totam suscipit atque dolorum repudiandae nostrum.',
         img:'./images/02.jpg'
